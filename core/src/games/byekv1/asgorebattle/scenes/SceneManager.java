1847aa3ff694769c380844232cd80589d576f550
kylebye4mav <kylebye4mav@gmail.com>
2020-12-19 01:48:48 -0600

    Started SceneManager

---
+++ core/src/games/byekv1/asgorebattle/scenes/SceneManager.java
@@ -0,0 +1,19 @@
+/**
+ * @author	byekv1
+ */
+package games.byekv1.asgorebattle.scenes;
+
+/**
+ * @author byekv1
+ *
+ */
+public class SceneManager {
+
+	/**
+	 * 
+	 */
+	public SceneManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
